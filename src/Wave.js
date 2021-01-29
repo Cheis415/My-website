8bf6ab9b15592631ba23e93cd43db5c0dbfe51cf
Chad Heisler <chadheisler5@gmail.com>
2021-01-28 16:50:11 -0800

    improved responsiveness on mobile

--- src/Wave.js
+++ src/Wave.js
@@ -10,7 +10,7 @@ function Wave() {
 
   return (
     <div className="wrap" >
-    <svg className="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <svg className="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 330">
         <path fill="#fff" fillOpacity="1" d="M0,128L60,128C120,128,240,128,360,144C480,160,600,192,720,202.7C840,213,960,203,1080,
         192C1200,181,1320,171,1380,165.3L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,
         360,320C240,320,120,320,60,320L0,320Z"></path>
